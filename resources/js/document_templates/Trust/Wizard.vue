7ee0033c4d06292d4e0d0392c832b341dd62143e
Dustin Woodard <dustin.woodard@gmail.com>
2021-10-07 08:22:50 -0600

    added rule

--- resources/js/document_templates/Trust/Wizard.vue
+++ resources/js/document_templates/Trust/Wizard.vue
@@ -9,28 +9,24 @@
             step="1">
             Client Info
           </v-stepper-step>
-
           <v-divider/>
-
           <v-stepper-step
             editable
             :complete="currentStep > 2"
-            :rules="[() => true]"
+            :rules="[
+              () => {return !!form.trust_name}
+            ]"
             step="2">
             Trust Basic Info
           </v-stepper-step>
-
           <v-divider/>
-
           <v-stepper-step
             editable
             :complete="currentStep > 3"
             step="3">
             Trustees
           </v-stepper-step>
-
           <v-divider/>
-
           <v-stepper-step
             step="4"
             editable>
@@ -42,12 +38,6 @@
           <v-stepper-content step="1">
             <v-card class="mb-12" color="grey lighten-5">
               <v-flex md-6>
-                <v-text-field
-                  v-model="form.trust_name"
-                  label="Trust Name"
-                  @blur="updateProject"
-                  @keydown.enter="updateProject"/>
-
                 <v-text-field
                   v-model="form.first_trustee"
                   label="First Trustee"
@@ -62,10 +52,13 @@
               Continue
             </v-btn>
           </v-stepper-content>
-
-
           <v-stepper-content step="2">
             <v-card class="mb-12">
+              <v-text-field
+                v-model="form.trust_name"
+                label="Trust Name"
+                @blur="updateProject"
+                @keydown.enter="updateProject"/>
               <v-menu
                 v-model="MenuDocumentCreated"
                 label="Document Created"
@@ -86,6 +79,18 @@
                 label="Settlor"
                 @blur="updateProject"
                 @keydown.enter="updateProject"/>
+
+              <v-text-field
+                v-model="form.settlor_gift"
+                label="Settlor Gift"
+                @blur="updateProject"
+                @keydown.enter="updateProject"/>
+
+              <v-text-field
+                v-model="form.term_of_trust"
+                label="Term Of Trust"
+                @blur="updateProject"
+                @keydown.enter="updateProject"/>
             </v-card>
 
             <v-btn
@@ -94,10 +99,13 @@
               Continue
             </v-btn>
           </v-stepper-content>
-
           <v-stepper-content step="3">
             <v-card class="mb-12">
-              <!-- -->
+              <v-text-field
+                v-model="form.trust_name"
+                label="TrustName"
+                @blur="updateProject"
+                @keydown.enter="updateProject"/>
             </v-card>
 
             <v-btn
@@ -106,7 +114,6 @@
               Continue
             </v-btn>
           </v-stepper-content>
-
           <v-stepper-content step="4">
             <v-card class="mb-12">
               <!-- -->
@@ -120,11 +127,14 @@
           </v-stepper-content>
         </v-stepper-items>
       </v-stepper>
+
+      <VJsoneditor v-model="form.data()" :plus="false" height="400px" @error="onError"/>
     </template>
   </div>
 </template>
 <script>
   import moment from 'moment';
+  import VJsoneditor from 'v-jsoneditor';
 
   export default {
     props: ['project', 'client'],
@@ -136,26 +146,25 @@
           trust_name: this.project?.document_data?.trust_name || this.project.name,
           first_trustee: this.project?.document_data?.first_trustee || `${this.client.first_name} ${this.client.last_name}`,
           settlor: this.project?.document_data?.settlor,
-          document_created_at: this.project?.document_data?.document_created_at || this.moment().format('YYYY-MM-DD')
+          document_created_at: this.project?.document_data?.document_created_at || this.moment().format('YYYY-MM-DD'),
+          mailing_address: this.project?.document_data?.mailing_address || {
+            address: null,
+            city: null,
+            state: null,
+            zip: null,
+            county: null
+          },
+          domicile_address: this.project?.document_data?.domicile_address || {
+            address: null,
+            city: null,
+            state: null,
+            zip: null,
+            county: null
+          },
+          settlor_gift: this.project?.document_data?.settlor_gift || '$100',
+          term_of_trust: this.project?.document_data?.term_of_trust || '99',
+          secondary_trustees: this.project?.document_data?.secondary_trustees || []
         })
-        // document_data: {
-        //   trust_name: null,
-        //   first_trustee: null,
-        //   mail_address: {
-        //     address: null,
-        //     city: null,
-        //     state: null,
-        //     zip: null,
-        //     county: null
-        //   },
-        //   meeting_date: null,
-        //   settlor: null,
-        //   settlor_gift: null,
-        //   term_of_trust: null,
-        //   secondary_trustees: [
-        //     'John Doe'
-        //   ]
-        // }
       };
     },
     methods: {
@@ -172,6 +181,9 @@
         const steps = this.$el.querySelectorAll('.v-stepper__step').length;
         this.currentStep = this.currentStep === steps ? 1 : this.currentStep + 1;
       }
+    },
+    components: {
+      VJsoneditor
     }
   };
 </script>
